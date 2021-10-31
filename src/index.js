828e5cc95ca627416165ee1732b9a36cdff24e22
Tsukyko <zoeamat.pro@gmail.com>
2021-10-31 14:15:17 +0100

    Config: add proptypes + chakra provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 
 ReactDOM.render(
+  <ChakraProvider>
     <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>,
+  document.getElementById('root')
 );
