bfb655702853ec09a6bd14d86155dabc691d377a
Tsukyko <zoeamat.pro@gmail.com>
2021-10-31 14:19:02 +0100

    Feature: NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  IconButton,
+  Stack,
+  Switch,
+  useColorMode,
+  useColorModeValue,
+  useDisclosure,
+} from '@chakra-ui/react';
+// import PropTypes from 'prop-types';
+import { CloseIcon, HamburgerIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
+import NavLink from './NavLink';
+
+const Links = ['Home', 'About', 'Skills', 'Projects', 'Contact'];
+
+const NavBar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={{ base: 4, lg: 8 }}>
+        <Flex h={16} alignItems="center" justifyContent="space-between">
+          <IconButton
+            size="md"
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label="Open Menu"
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack alignItems="center">
+            <HStack as="nav" spacing={8} display={{ base: 'none', md: 'flex' }}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems="baseline" justifyContent="space-between">
+            <Switch colorScheme="none" p="5" />
+            <Button onClick={toggleColorMode}>
+              {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+            </Button>
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as="nav" spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+};
+
+export default NavBar;
---
+++ src/components/NavLink.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, useColorModeValue } from '@chakra-ui/react';
+
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded="md"
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href="./"
+  >
+    {children}
+  </Link>
+);
+
+NavLink.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default NavLink;
