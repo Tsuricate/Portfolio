720d9af74f2e8ca9e9b4c4faf0c041c45d307dc0
Tsukyko <zoeamat.pro@gmail.com>
2021-11-03 15:47:22 +0100

    Data: list of all icons + name for skills

---
+++ src/data/skills.js
@@ -0,0 +1,124 @@
+const skills = [
+  {
+    title: 'Front-End',
+    list: [
+      {
+        name: 'HTML5',
+        icon: 'SiHtml5',
+      },
+      {
+        name: 'CSS3',
+        icon: 'SiCss3',
+      },
+      {
+        name: 'Javascript',
+        icon: 'SiJavascript',
+      },
+      {
+        name: 'React',
+        icon: 'SiReact',
+      },
+      {
+        name: 'Redux',
+        icon: 'SiRedux',
+      },
+      {
+        name: 'Bootstrap',
+        icon: 'SiBootstrap',
+      },
+      {
+        name: 'Chakra UI',
+        icon: 'SiChakraui',
+      },
+      {
+        name: 'Semantic UI',
+        icon: 'SiSemanticuireact',
+      },
+    ],
+  },
+  {
+    title: 'Back-end',
+    list: [
+      {
+        name: 'PHP',
+        icon: 'SiPhp',
+      },
+      {
+        name: 'MySQL',
+        icon: 'SiMysql',
+      },
+      {
+        name: 'NodeJS',
+        icon: 'SiNodedotjs',
+      },
+      {
+        name: 'Express',
+        icon: 'SiExpress',
+      },
+      {
+        name: 'Lumen',
+        icon: 'SiLumen',
+      },
+    ],
+  },
+  {
+    title: 'Tools',
+    list: [
+      {
+        name: 'Git',
+        icon: 'SiGit',
+      },
+      {
+        name: 'GitHub',
+        icon: 'SiGithub',
+      },
+      {
+        name: 'Agile & Scrum',
+        icon: 'SiHandshake',
+      },
+      {
+        name: 'Visual Studio Code',
+        icon: 'SiVisualstudiocode',
+      },
+      {
+        name: 'Slack',
+        icon: 'SiSlack',
+      },
+      {
+        name: 'Trello',
+        icon: 'SiTrello',
+      },
+      {
+        name: 'Jira',
+        icon: 'SiJirasoftware',
+      },
+    ],
+  },
+  {
+    title: 'Other',
+    list: [
+      {
+        name: 'Next',
+        icon: 'SiNextdotjs',
+      },
+      {
+        name: 'Vue',
+        icon: 'SiVuedotjs',
+      },
+      {
+        name: 'Nuxt',
+        icon: 'SiNuxtdotjs',
+      },
+      {
+        name: 'Typescript',
+        icon: 'SiTypescript',
+      },
+      {
+        name: 'Python',
+        icon: 'SiPython',
+      },
+    ],
+  },
+];
+
+export default skills;
