a5a5c608867ebd4289be51d1f7ef19a381f6a98c
Tsukyko <zoeamat.pro@gmail.com>
2021-11-03 18:17:01 +0100

    Data: projects infos

---
+++ src/data/projects.js
@@ -0,0 +1,96 @@
+export const projects = [
+  {
+    title: 'TaleMe',
+    image: 'https://cdn.pixabay.com/photo/2020/05/15/13/31/illumination-5173540_960_720.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis ac dui ut porta. Nunc a efficitur eros. Phasellus et efficitur libero. Nulla arcu odio, accumsan sed aliquet eget, lobortis ut eros. Fusce sed purus imperdiet, suscipit nunc sed, lacinia urna. ',
+    url: './',
+    skills: [
+      {
+        name: 'React',
+        icon: 'SiReact',
+      },
+      {
+        name: 'Redux',
+        icon: 'SiRedux',
+      },
+      {
+        name: 'React Router',
+        icon: 'SiReactrouter',
+      },
+      {
+        name: 'React Hook Form',
+      },
+      {
+        name: 'Axios',
+      },
+      {
+        name: 'JSON Web Token',
+        icon: 'SiJsonwebtokens',
+      },
+      {
+        name: 'Semantic UI',
+        icon: 'SiSemanticuireact',
+      },
+      {
+        name: 'Symfony',
+        icon: 'SiSymfony',
+      },
+      {
+        name: 'Doctrine',
+        icon: 'DiDoctrine',
+      },
+    ],
+  },
+  {
+    title: 'YAAC',
+    image: 'https://cdn.pixabay.com/photo/2020/05/19/13/32/cartoon-5190837_960_720.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis ac dui ut porta. Nunc a efficitur eros. Phasellus et efficitur libero. Nulla arcu odio, accumsan sed aliquet eget, lobortis ut eros. Fusce sed purus imperdiet, suscipit nunc sed, lacinia urna. ',
+    url: './',
+    skills: [
+      {
+        name: 'React',
+        icon: 'SiReact',
+      },
+      {
+        name: 'Axios',
+      },
+      {
+        name: 'Chakra UI',
+        icon: 'SiChakraui',
+      },
+      {
+        name: 'NodeJS',
+        icon: 'SiNodedotjs',
+      },
+      {
+        name: 'Express',
+        icon: 'SiExpress',
+      },
+      {
+        name: 'Google Drive API',
+        icon: 'SiGoogledrive',
+      },
+    ],
+  },
+  {
+    title: 'Portfolio',
+    image: 'https://cdn.pixabay.com/photo/2020/01/29/09/36/connection-4801974_960_720.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis ac dui ut porta. Nunc a efficitur eros. Phasellus et efficitur libero. Nulla arcu odio, accumsan sed aliquet eget, lobortis ut eros. Fusce sed purus imperdiet, suscipit nunc sed, lacinia urna. ',
+    url: './',
+    skills: [
+      {
+        name: 'React',
+        icon: 'SiReact',
+      },
+      {
+        name: 'Chakra UI',
+        icon: 'SiChakraui',
+      },
+    ],
+  },
+];
+
+export default projects;
