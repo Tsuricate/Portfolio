010ca7f4ec7b6d81a6cb3612efdcae00b743456d
Tsukyko <zoeamat.pro@gmail.com>
2021-11-03 15:48:31 +0100

    Feature: SkillsContent divided with grid

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import HomeContent from './components/HomeContent';
 import AboutContent from './components/AboutContent';
+import SkillsContent from './components/SkillsContent';
 
 const App = () => (
   <>
     <NavBar />
     <HomeContent />
     <AboutContent />
+    <SkillsContent />
   </>
 );
 
---
+++ src/components/SkillsContent.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Box, Grid, GridItem } from '@chakra-ui/react';
+import SectionContainer from './SectionContainer';
+import SkillsGroup from './SkillsGroup';
+import skills from '../data/skills';
+
+const firstGroup = skills.slice(0, 3);
+const secondGroup = skills.slice(-1);
+
+const SkillsContent = () => (
+  <SectionContainer bgColor="#F0EFF4" sectionTitle="Skills" fullWidth>
+    {/* Banner */}
+    <Box background="#90BE6D" height="170px" />
+    <Grid
+      templateColumns={{ base: 'repeat(1, 1fr)', lg: 'repeat(4, 1fr)' }}
+      gap={4}
+      height="calc(100% - 170px)"
+    >
+      <GridItem colSpan={3}>
+        <SkillsGroup groupTitle="I have already work with" skills={firstGroup} />
+      </GridItem>
+      <GridItem colSpan={1}>
+        <SkillsGroup groupTitle="But I’m also interested in ..." skills={secondGroup} />
+      </GridItem>
+    </Grid>
+  </SectionContainer>
+);
+
+export default SkillsContent;
