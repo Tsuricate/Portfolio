d31b42681f6962fde8e699f42653a22172b978cf
Tsukyko <zoeamat.pro@gmail.com>
2021-11-05 20:46:26 +0100

    Data: about sections : title + texts

---
+++ src/data/aboutSections.js
@@ -0,0 +1,25 @@
+const aboutSections = [
+  {
+    title: 'Background',
+    texts: [
+      'Proin vel lacinia ipsum, et lobortis lorem. Ut quis leo et nulla rhoncus pharetra. Proin consectetur lacus id pulvinar luctus. Vestibulum venenatis, mauris ac condimentum sagittis, nunc mi mollis nisi, id tristique odio quam vel nulla. Praesent sollicitudin massa vestibulum finibus ullamcorper. Donec venenatis sem at blandit viverra. Proin non porta dui, et volutpat magna. Aenean volutpat iaculis tellus, in auctor odio mattis in.',
+      'Vestibulum posuere justo metus, in volutpat quam sagittis id. Cras malesuada nunc diam, nec scelerisque massa ultricies condimentum. Nunc id tempor neque, ac sollicitudin erat. Maecenas sollicitudin posuere mauris, quis porttitor lacus scelerisque at. Praesent nisl leo, rhoncus in placerat nec, rutrum sed odio. Maecenas mattis ligula purus, sed tempus tellus porttitor quis. Aenean fermentum magna eget dolor efficitur, eget interdum enim venenatis. Nam id iaculis ex. Morbi id mattis nibh. Nulla finibus, magna id sagittis laoreet, elit justo consequat metus, et vehicula dui enim ac metus. Nulla dignissim et lectus sit amet hendrerit. Cras porttitor, ipsum quis dapibus aliquet, orci augue cursus enim, sit amet rutrum tortor orci et felis. Integer lacinia tellus ac metus accumsan, vel gravida massa semper. Fusce eleifend maximus ligula, eget laoreet justo sollicitudin in.',
+    ],
+  },
+  {
+    title: 'Values',
+    texts: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat metus at viverra interdum. Cras libero est, aliquam quis aliquam nec, vehicula at ante. Fusce gravida posuere nisl sodales feugiat. Phasellus mollis tortor a ligula egestas pulvinar. Aliquam vehicula velit non sapien tempor, ac varius leo commodo. Etiam convallis nunc at felis elementum pretium. Fusce et tristique arcu, non auctor massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam at nisl sit amet pulvinar. Suspendisse id sem non augue porta congue id eget quam.',
+      'Quisque dignissim, dolor elementum efficitur elementum, libero est aliquam mauris, sed sodales est arcu quis turpis. Ut urna lorem, rhoncus vel erat sit amet, aliquam viverra orci. Vivamus lacus urna, egestas vel auctor a, malesuada in libero. Proin aliquet posuere consectetur. Sed imperdiet nunc lacus, sit amet fringilla risus lobortis vitae. Maecenas condimentum posuere mauris ut fermentum. Etiam tempor eros neque, et suscipit lorem.',
+    ],
+  },
+  {
+    title: 'Hobbies',
+    texts: [
+      'Morbi commodo sodales molestie. Praesent ligula risus, facilisis non lobortis sed, mollis in nibh. Vivamus a est eget ante volutpat varius. Fusce tincidunt porttitor finibus. Nunc vel dapibus tortor, id fermentum sapien. Vivamus hendrerit consectetur ligula, non ornare justo efficitur fringilla. Fusce lacinia imperdiet tortor ut scelerisque. Morbi euismod varius risus ac volutpat. Cras at velit ut diam sollicitudin efficitur. Fusce ac dolor et lectus vestibulum iaculis. Pellentesque non efficitur velit. Donec purus tellus, tincidunt vitae sem sit amet, condimentum iaculis neque.',
+      'Nunc tempor auctor dolor, vel sodales urna tristique a. Nullam id ante tristique, sagittis eros eu, varius turpis. Etiam varius nisi a leo varius, at consequat lorem scelerisque. Suspendisse id arcu sit amet ex porta efficitur tincidunt ut libero. Donec volutpat sollicitudin nunc, et accumsan velit elementum ac. Proin libero urna, mattis ut dignissim nec, finibus sed neque. Quisque at massa eros.',
+    ],
+  },
+];
+
+export default aboutSections;
