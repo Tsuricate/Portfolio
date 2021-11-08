ac0522e0329dd888f8ac27c062ef865cf7102833
Tsukyko <zoeamat.pro@gmail.com>
2021-11-08 17:40:23 +0100

    Feature: contact action button leads to contact page

--- src/components/ActionButtons.js
+++ src/components/ActionButtons.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Stack, Button } from '@chakra-ui/react';
+import { HashLink } from 'react-router-hash-link';
 
 const ActionButtons = () => (
   <Stack direction={{ base: 'column', sm: 'row' }} spacing={{ base: 3, md: 8 }} pt={6}>
     <Button colorScheme="black" variant="outline" bg="white" p={6}>
       Download my resume
     </Button>
-    <Button colorScheme="white" bg="black" variant="solid" p={6}>
+    <Button as={HashLink} to="#Contact" smooth colorScheme="white" bg="black" variant="solid" p={6}>
       Contact me
     </Button>
   </Stack>
