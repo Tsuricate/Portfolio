05ef278a9b30073ce4cc56e30c031fa1acd737e3
Tsukyko <zoeamat.pro@gmail.com>
2021-11-15 15:38:54 +0100

    Add home page image

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -1,31 +1,36 @@
 import React from 'react';
-import { Flex, Heading, Text, Wrap, Box } from '@chakra-ui/react';
+import { Grid, GridItem, Heading, Text, Wrap, Box, Image } from '@chakra-ui/react';
 import SectionContainer from './SectionContainer';
 import ActionButtons from './ActionButtons';
+import test from '../data/test.png';
 
 const HomeContent = () => (
-  <SectionContainer bgColor="lightGray">
-    <Flex
-      height={{ base: '100%' }}
-      width={{ lg: '50%' }}
-      justifyContent={{ xl: 'center' }}
-      alignItems={{ xl: 'center' }}
-      py={{ base: 10, lg: 40 }}
+  <SectionContainer bgColor="white">
+    <Grid
+      templateColumns={{ base: 'repeat(1, 1fr)', xl: 'repeat(3, 1fr)' }}
+      height={{ xl: '100%' }}
+      justifyContent="center"
+      alignItems="baseline"
     >
-      <Wrap direction="column" spacing={{ base: 10 }}>
-        <Box>
-          <Text fontSize={{ base: '3xl' }}>Hi, I&apos;m Zoé.</Text>
-          <Heading fontSize={{ base: '4xl' }}>Front-end web developper</Heading>
-        </Box>
-        <Text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dolor lacus, dapibus quis
-          pharetra at, molestie nec lorem. Nullam sit amet maximus lectus, in auctor risus. Praesent
-          id velit ac ligula auctor accumsan. Phasellus consectetur enim dignissim lobortis
-          ultrices.
-        </Text>
-        <ActionButtons />
-      </Wrap>
-    </Flex>
+      <GridItem colSpan={{ base: 1, xl: 1 }}>
+        <Wrap direction="column" spacing={{ base: 10 }} mt={{ xl: 20 }}>
+          <Box>
+            <Text fontSize={{ base: '3xl' }}>Hi, I&apos;m Zoé.</Text>
+            <Heading fontSize={{ base: '4xl' }}>Front-end web developper</Heading>
+          </Box>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dolor lacus, dapibus quis
+            pharetra at, molestie nec lorem. Nullam sit amet maximus lectus, in auctor risus.
+            Praesent id velit ac ligula auctor accumsan. Phasellus consectetur enim dignissim
+            lobortis ultrices.
+          </Text>
+          <ActionButtons />
+        </Wrap>
+      </GridItem>
+      <GridItem colSpan={{ base: 1, xl: 2 }}>
+        <Image src={test} />
+      </GridItem>
+    </Grid>
   </SectionContainer>
 );
 
