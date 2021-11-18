17947d0708f5a8934451e6a67fb7bc2acb85a8f1
Tsukyko <zoeamat.pro@gmail.com>
2021-11-18 15:46:56 +0100

    Style: add images in about page

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   SimpleGrid,
-  Box,
   Tabs,
   Flex,
   TabList,
@@ -9,7 +8,8 @@ import {
   Tab,
   TabPanel,
   TabPanels,
-  VStack,
+  Stack,
+  Image,
 } from '@chakra-ui/react';
 import SectionContainer from './SectionContainer';
 import ActionButtons from './ActionButtons';
@@ -17,8 +17,23 @@ import aboutSections from '../data/aboutSections';
 
 const AboutContent = () => (
   <SectionContainer bgColor="#F0F7F4" sectionTitle="About">
-    <SimpleGrid columns={{ base: 1, xl: 2 }} height={{ xl: '100%' }} p={{ xl: 16 }}>
-      <Box>Image to come</Box>
+    <SimpleGrid
+      columns={{ base: 1, xl: 2 }}
+      height={{ xl: '100%' }}
+      gap={{ base: 5, md: 10, xl: 20 }}
+      alignItems={{ xl: 'center' }}
+    >
+      <SimpleGrid
+        columns={3}
+        direction="row"
+        justifyItems="center"
+        spacing={{ base: 1, md: 3 }}
+        p={{ xl: 10 }}
+      >
+        <Image src="/images/pano1.webp" width="100%" height="auto" objectFit="cover" />
+        <Image src="/images/pano2.webp" width="100%" height="auto" objectFit="cover" />
+        <Image src="/images/pano3.webp" width="100%" height="auto" objectFit="cover" />
+      </SimpleGrid>
 
       <Flex flexDirection="column" justifyContent={{ xl: 'center' }}>
         <Tabs variant="soft-rounded" colorScheme="purple" isFitted>
@@ -31,17 +46,18 @@ const AboutContent = () => (
           <TabPanels>
             {aboutSections.map((section) => (
               <TabPanel key={section.title}>
-                <VStack spacing={{ base: 5 }}>
+                <Stack direction="column" spacing={{ base: 5 }}>
                   {section.texts.map((text) => (
                     <Text key={text} lineHeight={1.7}>
                       {text}
                     </Text>
                   ))}
-                </VStack>
+                </Stack>
               </TabPanel>
             ))}
           </TabPanels>
         </Tabs>
+
         <ActionButtons />
       </Flex>
     </SimpleGrid>
