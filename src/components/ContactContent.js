9bd600ffac2c21a9a92333193117289666411ed6
Tsukyko <zoeamat.pro@gmail.com>
2021-11-24 15:19:52 +0100

    Style: add background image on Contact Page + center text

--- src/components/ContactContent.js
+++ src/components/ContactContent.js
@@ -1,29 +1,28 @@
-import {
-  Box,
-  Button,
-  Link,
-  SimpleGrid,
-  Stack,
-  Text,
-  LinkOverlay,
-  LinkBox,
-  Flex,
-  Image,
-} from '@chakra-ui/react';
+import { Button, Link, Stack, Text, Container } from '@chakra-ui/react';
 import React from 'react';
-import { FaAngleDoubleUp, FaLinkedin, FaRegEnvelope, FaUserCircle } from 'react-icons/fa';
-import { HashLink } from 'react-router-hash-link';
+import { FaLinkedin, FaRegEnvelope, FaUserCircle } from 'react-icons/fa';
 import SectionContainer from './SectionContainer';
 
 const ContactContent = () => (
-  <SectionContainer bgColor="#F9EBE0" sectionTitle="Contact">
-    <SimpleGrid columns={{ base: 1, lg: 2 }} height={{ xl: '100%' }}>
+  <SectionContainer
+    background={
+      'url("/images/leftHand.webp") no-repeat bottom left, url("/images/rightHand.webp") no-repeat top right #F9EBE0'
+    }
+    sectionTitle="Contact"
+  >
+    <Container
+      maxW={{ base: 'container.2xs', xl: 'container.lg' }}
+      centerContent
+      align="center"
+      height={{ xl: '100%' }}
+    >
       <Stack
         p={{ md: 8 }}
         direction="column"
         spacing={{ base: 8, md: 14 }}
         height={{ xl: '100%' }}
         justify={{ xl: 'space-evenly' }}
+        align={{ xl: 'center' }}
       >
         <Stack direction="column" spacing={{ base: 4, md: 8 }}>
           <Text fontSize={{ base: '2xl', xl: '4xl' }}>Let&apos;s get in touch !</Text>
@@ -36,7 +35,7 @@ const ContactContent = () => (
             lobortis.
           </Text>
         </Stack>
-        <Stack direction="column" spacing={5} align={{ lg: 'baseline' }}>
+        <Stack direction={{ base: 'column', xl: 'row' }} spacing={5} align={{ lg: 'baseline' }}>
           <Button
             as={Link}
             href="mailto:zoeamat.pro@gmail.com"
@@ -63,42 +62,7 @@ const ContactContent = () => (
           </Button>
         </Stack>
       </Stack>
-
-      <Box>
-        <Flex
-          height={{ xl: '100%' }}
-          justify={{ xl: 'space-between' }}
-          alignItems="center"
-          direction="column"
-          p={10}
-        >
-          <Box boxSize={{ base: '2xs', lg: 'md', xl: 'xl' }}>
-            <Image
-              src="https://cdn.pixabay.com/photo/2020/05/19/13/54/cartoon-5190961_960_720.jpg"
-              alt="Contact"
-              borderRadius="full"
-            />
-          </Box>
-          <Box alignSelf={{ lg: 'flex-end' }}>
-            <LinkBox>
-              <Stack
-                direction="column"
-                align="center"
-                p={5}
-                borderRadius={5}
-                mt={10}
-                bg="whiteAlpha.700"
-              >
-                <FaAngleDoubleUp />
-                <LinkOverlay as={HashLink} to="#" smooth>
-                  Back to top
-                </LinkOverlay>
-              </Stack>
-            </LinkBox>
-          </Box>
-        </Flex>
-      </Box>
-    </SimpleGrid>
+    </Container>
   </SectionContainer>
 );
 
