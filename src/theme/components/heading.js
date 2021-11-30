efc6fcbdbfcaf6828ae101f86766960ff75e9784
Tsukyko <zoeamat.pro@gmail.com>
2021-11-30 21:20:36 +0100

    Style : heading component customization

---
+++ src/theme/components/heading.js
@@ -0,0 +1,12 @@
+const Heading = {
+  baseStyle: {
+    letterSpacing: '4px',
+  },
+  variants: {
+    'page-title': {
+      textTransform: 'uppercase',
+    },
+  },
+};
+
+export default Heading;
