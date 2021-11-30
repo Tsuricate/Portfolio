c4bc3c9d84b706c7676f1df16c395b04de1afd7b
Tsukyko <zoeamat.pro@gmail.com>
2021-11-30 21:21:04 +0100

    Fix: inverted color on action buttons

--- src/components/ActionButtons.js
+++ src/components/ActionButtons.js
@@ -4,18 +4,16 @@ import { HashLink } from 'react-router-hash-link';
 
 const ActionButtons = () => (
   <Stack direction={{ base: 'column', sm: 'row' }} spacing={{ base: 3, md: 8 }} pt={6}>
-    <Button colorScheme="black" variant="outline" bg={useColorModeValue('white', 'black')} p={6}>
-      Download my resume
-    </Button>
     <Button
-      as={HashLink}
-      to="#Contact"
-      smooth
-      colorScheme="white"
-      bg={useColorModeValue('black', 'white')}
-      variant="solid"
+      colorScheme="black"
+      variant="outline"
+      color={useColorModeValue('black', 'white')}
+      bg={useColorModeValue('white')}
       p={6}
     >
+      Download my resume
+    </Button>
+    <Button as={HashLink} to="#Contact" smooth color="white" bg="black" variant="solid" p={6}>
       Contact me
     </Button>
   </Stack>
