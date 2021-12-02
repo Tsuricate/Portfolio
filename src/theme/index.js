3aa4b39dc3b675e926f54f5e797bb5a4bb6ad541
Tsukyko <zoeamat.pro@gmail.com>
2021-12-02 20:55:15 +0100

    Chore: upgrade chakra + refacto theme files

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,12 +1,10 @@
 import { extendTheme } from '@chakra-ui/react';
 import fonts from './fonts';
-import Heading from './components/heading';
+import components from './components';
 
 const overrides = {
   fonts,
-  components: {
-    Heading,
-  },
+  components,
 };
 
 export default extendTheme(overrides);
