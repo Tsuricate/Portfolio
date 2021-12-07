e3e6b9c7022ecd6d04d96ebffb160097f437e787
Tsukyko <zoeamat.pro@gmail.com>
2021-12-07 14:29:28 +0100

    Style: hand drawing border style for skills lists

--- src/components/SkillsList.js
+++ src/components/SkillsList.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Stack, Text, Wrap } from '@chakra-ui/react';
+import { Stack, Heading, Wrap } from '@chakra-ui/react';
 import SkillTag from './SkillTag';
 
 const SkillsList = ({ category, skillList }) => (
@@ -9,13 +9,27 @@ const SkillsList = ({ category, skillList }) => (
     width={{ base: 'full', xl: 'auto' }}
     minHeight={{ xl: 'sm' }}
     p={{ base: 3, md: 4, xl: 5 }}
-    borderRadius="10px"
+    bg="#D9D9D3"
     spacing={5}
-    border="2px solid #F4A259"
+    border="2px solid #AAAA9D"
+    borderRadius="2% 6% 5% 4% / 1% 1% 2% 4%"
+    position="relative"
+    _before={{
+      content: "''",
+      border: '2px solid #AAAA9D',
+      display: 'block',
+      width: '100%',
+      height: '100%',
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate3d(-49%, -49%, 0) scale(1.015) rotate(0.5deg)',
+      borderRadius: ' 1% 1% 2% 4% / 2% 6% 5% 4%',
+    }}
   >
-    <Text textAlign="center" fontWeight="bold">
+    <Heading as="h4" textAlign="center" fontSize="lg">
       {category}
-    </Text>
+    </Heading>
     <Wrap spacing={5} justify="center">
       {skillList.map((skill) => (
         <SkillTag key={skill.name} name={skill.name} icon={skill.icon} />
