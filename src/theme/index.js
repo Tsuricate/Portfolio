4824960cc6cca321b753d8ee1c55fc6e4fd16fe4
Tsukyko <zoeamat.pro@gmail.com>
2021-12-08 19:24:27 +0100

    Fix: change xl breakpoint in theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,9 +1,11 @@
 import { extendTheme } from '@chakra-ui/react';
 import fonts from './fonts';
+import breakpoints from './breakpoints';
 import components from './components';
 
 const overrides = {
   fonts,
+  breakpoints,
   components,
 };
 
