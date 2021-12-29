88e219904880b13a4dda0f9885028ab9cc5d70fa
Tsukyko <zoeamat.pro@gmail.com>
2021-12-29 15:40:53 +0100

    Config: init AOS + create four simple fade animations + config eslint for props spreading

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import theme from './theme';
 import './i18n';
 
 import App from './App';
+
+AOS.init({
+  once: true,
+});
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/utils/animations.js
@@ -0,0 +1,30 @@
+export const fade = {
+  'data-aos': 'fade',
+  'data-aos-duration': '600',
+  'data-aos-easing': 'ease-in-out-cubic',
+  'data-aos-anchor-placement': 'top-bottom',
+};
+
+export const fadeDown = {
+  'data-aos': 'fade-down',
+  'data-aos-duration': '500',
+  'data-aos-delay': '100',
+  'data-aos-easing': 'ease-in-out-cubic',
+  'data-aos-anchor-placement': 'top-bottom',
+};
+
+export const fadeRight = {
+  'data-aos': 'fade-right',
+  'data-aos-duration': '500',
+  'data-aos-delay': '300',
+  'data-aos-easing': 'ease-in-out-cubic',
+  'data-aos-anchor-placement': 'top-bottom',
+};
+
+export const fadeUp = {
+  'data-aos': 'fade-up',
+  'data-aos-duration': '400',
+  'data-aos-delay': '400',
+  'data-aos-easing': 'ease-in-out-cubic',
+  'data-aos-anchor-placement': 'top-bottom',
+};
