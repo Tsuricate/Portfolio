ae8b24550c0901085ab57eb89bec8d18866d4c49
Tsukyko <zoeamat.pro@gmail.com>
2022-01-03 17:19:23 +0100

    Feature: download resume

--- src/components/ActionButtons.js
+++ src/components/ActionButtons.js
@@ -5,7 +5,15 @@ import { HashLink } from 'react-router-hash-link';
 import { fadeUp } from '../utils/animations';
 
 const ActionButtons = () => {
-  const { t } = useTranslation();
+  const { i18n, t } = useTranslation();
+
+  const downloadResume = () => {
+    const resume = i18n.language === 'fr' ? 'CV_Zoe_AMAT.pdf' : 'Resume_Zoe_AMAT.pdf';
+    const link = document.createElement('a');
+    link.download = resume;
+    link.href = resume;
+    link.click();
+  };
   return (
     <Stack
       direction={{ base: 'column', sm: 'row' }}
@@ -20,9 +28,11 @@ const ActionButtons = () => {
         bg={useColorModeValue('white')}
         p={6}
         isFullWidth={{ md: true }}
+        onClick={downloadResume}
       >
         {t('buttons.resume')}
       </Button>
+
       <Button
         as={HashLink}
         to="#Contact"
